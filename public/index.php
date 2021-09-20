9508a56f928ba06a5010e859ce3bba66080c25ab
istiaqhossain <md.istiaqhossain1990@gmail.com>
2021-09-20 06:01:13 +0600

    Implement views & layouts

--- public/index.php
+++ public/index.php
@@ -4,8 +4,10 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use app\core\Application;
 
-$app = new Application();
-$app->router->get('/',function(){
-    return 'Hello World';
+$app = new Application(dirname(__DIR__));
+$app->router->get('/','home');
+$app->router->get('/contact','contact');
+$app->router->post('/contact',function(){
+    
 });
 $app->run();
